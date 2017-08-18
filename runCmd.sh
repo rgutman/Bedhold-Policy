ca95b7fe7925c117202397984e5793936bf20c73
rgutman <31135004+rgutman@users.noreply.github.com>
2017-08-18 13:01:14 -0400

    Add files via upload

---
+++ runCmd.sh
@@ -0,0 +1,10 @@
+nohup R CMD BATCH --no-save '--args minFacID=1 maxFacID=50' SyntheticSimulated.r synLog1.Rout &
+nohup R CMD BATCH --no-save '--args minFacID=51 maxFacID=100' SyntheticSimulated.r synLog2.Rout &
+nohup R CMD BATCH --no-save '--args minFacID=101 maxFacID=150' SyntheticSimulated.r synLog3.Rout &
+nohup R CMD BATCH --no-save '--args minFacID=151 maxFacID=200' SyntheticSimulated.r synLog4.Rout &
+nohup R CMD BATCH --no-save '--args minFacID=201 maxFacID=250' SyntheticSimulated.r synLog5.Rout &
+nohup R CMD BATCH --no-save '--args minFacID=251 maxFacID=300' SyntheticSimulated.r synLog6.Rout &
+nohup R CMD BATCH --no-save '--args minFacID=301 maxFacID=350' SyntheticSimulated.r synLog7.Rout &
+nohup R CMD BATCH --no-save '--args minFacID=351 maxFacID=400' SyntheticSimulated.r synLog8.Rout &
+nohup R CMD BATCH --no-save '--args minFacID=401 maxFacID=450' SyntheticSimulated.r synLog9.Rout &
+nohup R CMD BATCH --no-save '--args minFacID=451 maxFacID=500' SyntheticSimulated.r synLog10.Rout &
